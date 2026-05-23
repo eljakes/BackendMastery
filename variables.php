cad8fc73a5770ef41e2c17f069f6e3c2de0589a8
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-05-23 05:27:26 +0000

    Learned PHP variables and Data types

---
+++ variables.php
@@ -0,0 +1,44 @@
+<?php
+
+$name = "Elvis";
+$age = 25;
+$isDeveloper = true;
+
+echo $name;
+echo "<br>";
+echo $age;
+echo "<br>";
+echo $isDeveloper;
+echo "<br>";
+
+$skills = ["PHP", "PostgreSQL", "Git"];
+
+print_r($skills);
+
+echo "<br>";
+
+$firstName = "Elvis";
+$lastName = "Owusu";
+
+echo $firstName . " " . $lastName;
+
+echo "<br>";
+var_dump($name);
+echo "<br>";
+var_dump($age);
+echo "<br>";
+var_dump($isDeveloper);
+
+echo "<br>";
+$productName = "Samsung";
+$productPrice = 999.99;
+$inStock = true;
+$categories = ["Electronics", "Computers"];
+
+echo $productName;
+echo "<br>";
+echo $productPrice;
+echo "<br>";
+echo $inStock;
+echo "<br>";
+print_r($categories);
