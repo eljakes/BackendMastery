768856cd267899d9110171c0e0d4c1941488bbdf
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-05-23 06:26:42 +0000

    commit condi and login

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+
+$email = "nana@gmail.com";
+$password = "12345";
+
+if($email == "nana@gmail.com" && $password == "12345") {
+    echo "Welcome back!";
+} else {
+    echo "Try again!";
+}
+
