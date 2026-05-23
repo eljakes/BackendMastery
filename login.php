2acc5614ffc4a5becb80c804d69cef46c7aa5319
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-05-23 06:32:51 +0000

    Learned conditionals and backend decision logic

--- login.php
+++ login.php
@@ -2,8 +2,9 @@
 
 $email = "nana@gmail.com";
 $password = "12345";
+$isActive = true;
 
-if($email == "nana@gmail.com" && $password == "12345") {
+if($email == "nana@gmail.com" && $password == "12345" && $isActive == true) {
     echo "Welcome back!";
 } else {
     echo "Try again!";
